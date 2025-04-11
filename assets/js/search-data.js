dfedf740290810c65c75339c17204617c4e99c06
minjaechoics <minjaechoi.cs@gmail.com>
2025-04-11 15:58:33 +0000

    Deploying to gh-pages from @ minjaechoics/minjaechoics.github.io@84ff06d599abc5a0a770ed103b19f8ec0ca3dd05 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -388,8 +388,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-markdown-emoji-test-sparkles-smile",
+          title: 'Markdown emoji test :sparkles: :smile:',
           description: "",
           section: "News",},{id: "news-test",
           title: 'test',
